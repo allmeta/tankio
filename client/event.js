10e87523abd1c0d3a4be70bcfb8a0a5d4df9b3e0
Allmeta <thomasdasd@gmail.com>
2019-10-13 02:54:22 +0200

    events client

--- client/event.js
+++ client/event.js
@@ -1,14 +1,17 @@
-import { addPlayer, game, socket } from "./lib"
+import { addPlayer, game, socket } from "./lib.js"
+
 socket.on("newPlayer", (player) => addPlayer(player))
 socket.on("currentPlayers", (players) =>
   Object.entries(players).forEach(([key, val]) => addPlayer(val))
 )
-socket.on(
-  "playerMove",
-  ({ id, ...props }) => (game.players[id] = { ...props, ...game.players[id] })
-)
-socket.on(
-  "playerRotate",
-  ({ id, ...props }) => (game.players[id] = { ...props, ...game.players[id] })
-)
+socket.on("playerMove", ({ id, x, y }) => {
+  if (!(id in game.players)) return
+  game.players[id].x = x
+  game.players[id].y = y
+})
+socket.on("playerRotate", ({ id, r }) => {
+  if (!(id in game.players)) return
+  game.players[id].r = r
+})
 // socket.on("playerBullet")
+socket.on("playerDisconnect", (id) => delete game.players[id])
